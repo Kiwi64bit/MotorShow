e5c46fb1e2942dadcff040aea5243cbf5f97a0ff
Kiwi64bit <207353425+Kiwi64bit@users.noreply.github.com>
2026-04-29 16:22:40 +0300

    feat(theme): theme is now saved to local storage

--- scripts/toggleTheme.js
+++ scripts/toggleTheme.js
@@ -1,12 +1,40 @@
-// toggles the theme by changing `<html>` element `data-theme` attribute between "light" and "dark"
-function toggleTheme(event) {
-    const html = document.documentElement;
-    const currentTheme = html.getAttribute("data-theme") || "light";
-    const newTheme = currentTheme == "light" ? "dark" : "light";
-    html.setAttribute("data-theme", newTheme);
+// gets the page theme
+function getTheme() {
+    const theme = document.documentElement.getAttribute("data-theme");
+    return theme || "light";
 }
 
-// an array of buttons with the `theme-toggler` class
+// saves the current theme to local storage
+function saveTheme() {
+    const theme = getTheme();
+    localStorage.setItem("theme", theme);
+}
+
+// sets the page theme
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    saveTheme(theme);
+}
+
+// loads the theme from local storage
+function loadTheme() {
+    const theme = localStorage.getItem("theme") || "light";
+    setTheme(theme);
+}
+
+// toggles the theme between "light" and "dark"
+function toggleTheme() {
+    const currentTheme = getTheme();
+    if (currentTheme == "light") {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
+}
+
+loadTheme();
+
+// an array of all `theme-toggler` buttons in the page
 const themeButtons = document.querySelectorAll(".theme-toggler");
 
 // loop through each button and add an event listener
