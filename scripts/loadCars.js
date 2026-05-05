bd48697291102b42980ed684eac4918465ba245e
Kiwi64bit <207353425+Kiwi64bit@users.noreply.github.com>
2026-05-05 12:00:52 +0300

    fix(loadCars.js): somebody changed the file with out my permission

--- scripts/loadCars.js
+++ scripts/loadCars.js
@@ -1,58 +1,62 @@
-const resultsContainer = document.getElementById("resultsContainer");
-
-fetch("../db.json")
-    .then(res => res.json())
-    .then(data => {
-        const allCars = data.cars;
-
-        resultsContainer.innerHTML = "";
-
-        allCars.forEach((car, i) => {
-            const card = document.createElement("div");
-            card.classList.add("car-card");
-
-            card.innerHTML = `
-                <div class="card-img">
-                    <img src="../${car.image.replace("./", "")}" alt="${car.name}">
+function createCarCard(car) {
+    const card = document.createElement("div");
+    card.classList.add("car-card");
+    card.innerHTML = `
+        <div class="card-img">
+            <img src="../${car.image.replace("./", "")}" alt="${car.name}">
+        </div>
+        <div class="card-body">
+            <div class="car-head">
+                <h4>${car.name}</h4>
+            </div>
+            <div class="car-features">
+                <div class="car-feature">
+                    <i class="fa-solid fa-gauge"></i>
+                    <span>${car.miles.toLocaleString()} mi</span>
                 </div>
-                <div class="card-body">
-                    <div class="car-head">
-                        <h4>${car.name}</h4>
-                    </div>
-                    <div class="car-features">
-                        <div class="car-feature">
-                            <i class="fa-solid fa-gauge"></i>
-                            <span>${car.miles.toLocaleString()} mi</span>
-                        </div>
-                        <div class="car-feature">
-                            <i class="fa-solid fa-gas-pump"></i>
-                            <span>${car.fuel}</span>
-                        </div>
-                        <div class="car-feature">
-                            <i class="fa-solid fa-gears"></i>
-                            <span>${car.transmission}</span>
-                        </div>
-                    </div>
-                    <div class="car-details">
-                        <div class="car-price">
-                            ${car.price.toLocaleString("en-EG", {
-                                style: "currency",
-                                currency: "EGP",
-                                maximumFractionDigits: 0,
-                            })}
-                        </div>
-                        <a href="listing_details.html?id=${car.id}" class="view-btn">
-                            Details <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                        </a>
-                    </div>
+                <div class="car-feature">
+                    <i class="fa-solid fa-gas-pump"></i>
+                    <span>${car.fuel}</span>
                 </div>
-            `;
+                <div class="car-feature">
+                    <i class="fa-solid fa-gears"></i>
+                    <span>${car.transmission}</span>
+                </div>
+            </div>
+            <div class="car-details">
+                <div class="car-price">
+                    ${car.price.toLocaleString("en-EG", {
+                        style: "currency",
+                        currency: "EGP",
+                        maximumFractionDigits: 0,
+                    })}
+                </div>
+                <a href="listing_details.html?id=${car.id}" class="view-btn">
+                    Details <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
+            </div>
+        </div>
+    `;
 
-            resultsContainer.appendChild(card);
+    return card;
+}
 
-            setTimeout(() => {
-                card.classList.add("show");
-            }, i * 100);
-        });
+function loadCars(container, carsData) {
+    container.innerHTML = "";
+    for (const car of carsData) {
+        const card = createCarCard(car);
+        container.appendChild(card);
+    }
+}
+
+var resultsContainer = document.getElementById("resultsContainer");
+var cars;
+
+fetchJson("../db.json")
+    .then(result => {
+        cars = result.cars;
+        return result;
     })
-    .catch(error => console.log(error));
+    .then(result => {
+        loadCars(resultsContainer, cars);
+    });
