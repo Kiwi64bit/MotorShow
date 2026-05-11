97ccc59ac2d1221a3eb335b4e567598424a54720
Kiwi64bit <207353425+Kiwi64bit@users.noreply.github.com>
2026-05-12 00:47:33 +0300

    fix(listings)

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,43 +1,16 @@
 const filterForm = document.getElementById("filterOptions");
+const filterBrand = document.getElementById("brand");
+const filterModel = document.getElementById("model");
+const filterYear = document.getElementById("year");
+const filterPrice = document.getElementById("price");
 
-filterForm.addEventListener("submit", event => {
-    event.preventDefault();
-    const filters = getInputFields(filterForm);
-    const priceRange = parsePrice(filters.price);
-
-    filteredCars = cars.filter(car => {
-        const [brand, model] = car.name.split(" ");
-        const price = Number(car.price);
-
-        const matchBrand = !filters.brand || brand.toLowerCase() === filters.brand.toLowerCase();
-        const matchModel = !filters.model || model.toLowerCase() === filters.model.toLowerCase();
-        const matchYear = !filters.year || car.year == filters.year;
-        const matchPrice = priceRange.min <= price && price <= priceRange.max;
-
-        return matchBrand && matchModel && matchYear && matchPrice;
-    });
-
-    loadCars(resultsContainer, filteredCars);
-    resultsCount.textContent = `showing ${filteredCars.length} results`;
-});
+const resultsContainer = document.getElementById("resultsContainer");
+const resultsCount = document.getElementById("resultCount");
 
 const searchbar = document.getElementById("searchbar");
-
-searchbar.addEventListener("input", event => {
-    const query = searchbar.value.toLowerCase().trim();
-
-    const result = filteredCars.filter(car => {
-        const matchName = car.name.toLowerCase().includes(query);
-        const matchYear = car.year.toString().includes(query);
-
-        return matchName || matchYear;
-    });
-
-    loadCars(resultsContainer, result);
-    resultsCount.textContent = `showing ${result.length} results`;
-});
-
 const sortBy = document.getElementById("sortBy");
+const layoutSelect = document.getElementById("layoutSelect");
+const listingsContainer = document.getElementById("listingsContainer");
 
 const sortConditions = {
     priceAscending: (a, b) => a.price - b.price,
@@ -47,6 +20,55 @@ const sortConditions = {
     alphabetical: (a, b) => a.name.localeCompare(b.name),
 };
 
+var cars;
+var filteredCars;
+
+fetch("../db.json")
+    .then(response => response.json())
+    .then(result => {
+        cars = result.sort((a, b) => a.name.localeCompare(b.name));
+        filteredCars = [...cars];
+        loadCars(resultsContainer, cars);
+        resultsCount.textContent = `showing ${cars.length} results`;
+    })
+    .catch(error => console.error(error));
+
+filterForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const priceRange = parsePrice(filterPrice.value);
+
+    filteredCars = cars.filter(car => {
+        const [brand, model] = car.name.split(" ");
+        const price = Number(car.price);
+
+        const matchBrand = !filterBrand.value || brand.toLowerCase() === filterBrand.toLowerCase();
+        const matchModel = !filterModel || model.toLowerCase() === filterModel.toLowerCase();
+        const matchYear = !filterYear.value || car.year == filterYear;
+        const matchPrice = priceRange.min <= price && price <= priceRange.max;
+
+        return matchBrand && matchModel && matchYear && matchPrice;
+    });
+
+    loadCars(resultsContainer, filteredCars);
+    resultsCount.textContent = `showing ${filteredCars.length} results`;
+});
+
+searchbar.addEventListener("input", event => {
+    const query = searchbar.value.toLowerCase().trim();
+
+    const result = filteredCars.filter(car => {
+        const matchName = car.name.toLowerCase().includes(query);
+        const matchYear = car.year.toString().includes(query);
+        const matchTransmission = car.transmission.toLowerCase().includes(query);
+        const matchFuel = car.fuel.toLowerCase().includes(query);
+
+        return matchName || matchYear || matchTransmission || matchFuel;
+    });
+
+    loadCars(resultsContainer, result);
+    resultsCount.textContent = `showing ${result.length} results`;
+});
+
 sortBy.addEventListener("change", event => {
     const sortName = event.target.value;
     const condition = sortConditions[sortName];
@@ -54,9 +76,6 @@ sortBy.addEventListener("change", event => {
     const result = filteredCars.sort(condition);
     loadCars(resultsContainer, result);
 });
-
-const layoutSelect = document.getElementById("layoutSelect");
-const listingsContainer = document.getElementById("listingsContainer");
 
 layoutSelect.addEventListener("change", event => {
     const layout = event.target.value;
@@ -69,24 +88,6 @@ layoutSelect.addEventListener("change", event => {
         listingsContainer.classList.add("sidebar");
     }
 });
-
-function getInputFields(container) {
-    if (!container) return {};
-
-    const inputFields = container.querySelectorAll("input, select");
-    const data = {};
-
-    for (const field of inputFields) {
-        const key = field.name;
-        const value = field.value.trim();
-
-        if (key) {
-            data[key] = value;
-        }
-    }
-
-    return data;
-}
 
 function parsePrice(str) {
     if (typeof str !== "string") return { min: null, max: null };
@@ -112,4 +113,55 @@ function parsePrice(str) {
         min: value,
         max: value,
     };
+}
+
+function createCarCard(car) {
+    const card = document.createElement("div");
+    card.classList.add("car-card");
+    card.innerHTML = `
+        <div class="card-img">
+            <img src="../${car.image.replace("./", "")}" alt="${car.name}">
+        </div>
+        <div class="card-body">
+            <div class="car-head">
+                <h4>${car.name} ${car.year}</h4>
+            </div>
+            <div class="car-features">
+                <div class="car-feature">
+                    <i class="fa-solid fa-gauge"></i>
+                    <span>${car.miles.toLocaleString()} mi</span>
+                </div>
+                <div class="car-feature">
+                    <i class="fa-solid fa-gas-pump"></i>
+                    <span>${car.fuel}</span>
+                </div>
+                <div class="car-feature">
+                    <i class="fa-solid fa-gears"></i>
+                    <span>${car.transmission}</span>
+                </div>
+            </div>
+            <div class="car-details">
+                <div class="car-price">
+                    ${car.price.toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                        maximumFractionDigits: 0,
+                    })}
+                </div>
+                <a href="listing_details.html?id=${car.id}" class="view-btn">
+                    Details <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
+            </div>
+        </div>
+    `;
+
+    return card;
+}
+
+function loadCars(container, carsData) {
+    container.innerHTML = "";
+    for (const car of carsData) {
+        const card = createCarCard(car);
+        container.appendChild(card);
+    }
 }
